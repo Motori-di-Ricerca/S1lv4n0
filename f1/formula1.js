0a5e561d2dd7def223de6510078803dfb40dc463
KameNoEma <emanuele.scaltri@gmail.com>
2025-06-14 18:29:38 +0200

    Add files via upload

--- f1/formula1.js
+++ f1/formula1.js
@@ -9,201 +9,226 @@ window.addEventListener("DOMContentLoaded", function () {
         },
         {
             id: 5,
-            logo: "img/arabiasaudita.jpg", // correggi da .ipg a .jpg
+            logo: "img/arabiasaudita.jpg",
             gp: "GP Arabia Saudita",
             data: "18 - 20 aprile 2025",
             luogo: "Jeddah Street Circuit"
         },
         {
             id: 1,
-            logo: "img/australia.jpg", // correggi da .ipg a .jpg
+            logo: "img/australia.jpg",
             gp: "GP Australia",
             data: "14 - 16 marzo 2025",
             luogo: "Albert Park Circuit"
         },
         {
             id: 11,
-            logo: "img/austria.jpg", // correggi da .ipg a .jpg
+            logo: "img/austria.jpg",
             gp: "GP Austria",
             data: "27 - 29 giugno 2025",
             luogo: "Red Bull Ring"
         },
         {
             id: 17,
-            logo: "img/azerbaijan.jpg", // correggi da .ipg a .jpg
+            logo: "img/azerbaijan.jpg",
             gp: "GP Azerbaijan",
             data: "19 - 21 settembre 2025",
             luogo: "Baku street circuit"
         },
         {
             id: 4,
-            logo: "img/bahrain.jpg", // correggi da .ipg a .jpg
+            logo: "img/bahrain.jpg",
             gp: "GP Bahrain",
             data: "11 - 13 aprile 2025",
             luogo: "Bahrain International Circuit"
         },
         {
             id: 13,
-            logo: "img/belgio.jpg", // correggi da .ipg a .jpg
+            logo: "img/belgio.jpg",
             gp: "GP Belgio",
             data: "4 - 6 luglio 2025",
             luogo: "Spa-Francorchamps Circuit"
         },
         {
             id: 21,
-            logo: "img/brasile.jpg", // correggi da .ipg a .jpg
+            logo: "img/brasile.jpg",
             gp: "GP Brasile ",
-            data: "7 - 9 anovembre 2025",
+            data: "7 - 9 novembre 2025",
             luogo: "Autodromo Interlagos"
         },
         {
             id: 10,
-            logo: "img/canada.jpg", // correggi da .ipg a .jpg
+            logo: "img/canada.jpg",
             gp: "GP Canada ",
             data: "13 - 15 giugno 2025",
             luogo: "Circuit Gilles Villeneuve"
         },
         {
             id: 2,
-            logo: "img/cina.jpg", // correggi da .ipg a .jpg
+            logo: "img/cina.jpg",
             gp: "GP Cina",
             data: "21 - 23 marzo 2025",
             luogo: "Shanghai International Circuit"
         },
         {
             id: 3,
-            logo: "img/giappone.jpg", // correggi da .ipg a .jpg
+            logo: "img/giappone.jpg",
             gp: "GP Giappone",
             data: "4 - 6 aprile 2025",
             luogo: "Suzuka Circuit"
         },
         {
             id: 12,
-            logo: "img/granbretagna.jpg", // correggi da .ipg a .jpg
+            logo: "img/granbretagna.jpg",
             gp: "GP Gran Bretagna",
             data: "04 - 06 luglio 2025",
             luogo: "Silverstone Circuit"
         },
         {
             id: 7,
-            logo: "img/italia.jpg", // correggi da .ipg a .jpg
+            logo: "img/italia.jpg",
             gp: "GP Emilia Romagna",
             data: "16 - 18 maggio 2025",
             luogo: "Autodromo Imola"
         },
         {
             id: 16,
-            logo: "img/italia.jpg", // correggi da .ipg a .jpg
+            logo: "img/italia.jpg",
             gp: "GP Italia",
             data: "5 - 7 settembre 2025",
             luogo: "Autodromo Nazionale Monza"
         },
         {
             id: 22,
-            logo: "img/lasvegas.jpg", // correggi da .ipg a .jpg
+            logo: "img/lasvegas.jpg",
             gp: "GP Las Vegas",
             data: "20 - 22 novembre 2025",
             luogo: "Las Vegas Strip Circuit"
         },
         {
             id: 6,
-            logo: "img/maiami.jpg", // correggi da .ipg a .jpg
+            logo: "img/maiami.jpg",
             gp: "GP Miami",
             data: "2 - 4 maggio 2025",
             luogo: "Miami International Autodrome"
         },
         {
             id: 20,
-            logo: "img/messico.jpg", // correggi da .ipg a .jpg
+            logo: "img/messico.jpg",
             gp: "GP Messico",
             data: "24 - 26 ottobre 2025",
             luogo: "Autodromo Hermanos Rodriguez"
         },
         {
             id: 8,
-            logo: "img/monaco.jpg", // correggi da .ipg a .jpg
+            logo: "img/monaco.jpg",
             gp: "GP Monaco ",
             data: "23 - 25 maggio 2025",
             luogo: "Monaco Circuit"
         },
         {
             id: 15,
-            logo: "img/olanda.jpg", // correggi da .ipg a .jpg
+            logo: "img/olanda.jpg",
             gp: "GP Olanda",
             data: "29 - 31 agosto 2025",
             luogo: "Circuit Zandvoort"
         },
         {
             id: 23,
-            logo: "img/qatar.jpg", // correggi da .ipg a .jpg
+            logo: "img/qatar.jpg",
             gp: "GP Qatar",
             data: "28 - 30 novembre 2025",
             luogo: "Lusail International Circuit"
         },
         {
             id: 18,
-            logo: "img/singapore.jpg", // correggi da .ipg a .jpg
+            logo: "img/singapore.jpg",
             gp: "GP Singapore",
             data: "3 - 5 ottobre 2025",
             luogo: "Singapore Circuit"
         },
         {
             id: 9,
-            logo: "img/spagna.jpg", // correggi da .ipg a .jpg
+            logo: "img/spagna.jpg",
             gp: "GP Spagna",
             data: "30 maggio - 1 giugno 2025",
             luogo: "Catalunya Circuit"
         },
         {
             id: 14,
-            logo: "img/ungheria.jpg", // correggi da .ipg a .jpg
+            logo: "img/ungheria.jpg",
             gp: "GP Ungheria",
             data: "1 - 3 agosto 2025",
-            luogo: "	Hungaroring"
+            luogo: "Hungaroring"
         },
         {
             id: 19,
-            logo: "img/usa.jpg", // correggi da .ipg a .jpg
+            logo: "img/usa.jpg",
             gp: "GP Usa",
             data: "17 - 19 ottobre 2025",
             luogo: "Circuit of The Americas"
-        },
-
+        }
     ];
 
-    // Ordina per ID (crescente)
     calendarData.sort((a, b) => a.id - b.id);
 
-    // Dividi in due gruppi da 12
-    const firstHalf = calendarData.slice(0, 12);
-    const secondHalf = calendarData.slice(12);
+    const container = document.querySelector(".calendar-container");
 
-    // Seleziona i due tbody
-    const tbodyLeft = document.querySelector("#f1-calendar-left tbody");
-    const tbodyRight = document.querySelector("#f1-calendar-right tbody");
+    const leftTable = document.createElement("table");
+    const rightTable = document.createElement("table");
 
-    // Funzione per popolare una tabella
-    function populateTable(data, tbody) {
-        data.forEach(item => {
-            const row = document.createElement("tr");
-            row.innerHTML = `
-                <td><img src="${item.logo}" alt="Logo GP" "></td>
-                <td>${item.gp}</td>
-                <td>${item.data}</td>
-                <td><a href="#" onclick="showImagePopup('${getImageFilenameFromGP(item.gp)}', '${item.luogo}'); return false;">${item.luogo}</a></td>
-            `;
-            tbody.appendChild(row);
-        });
+    const createHeader = () => {
+        const thead = document.createElement("thead");
+        thead.innerHTML = `
+            <tr>
+                <th>Stato</th>
+                <th>Gran Premio</th>
+                <th>Data</th>
+                <th>Tracciato</th>
+            </tr>
+        `;
+        return thead;
+    };
+
+    leftTable.appendChild(createHeader());
+    rightTable.appendChild(createHeader());
+
+    const leftBody = document.createElement("tbody");
+    const rightBody = document.createElement("tbody");
+
+    calendarData.forEach((item, index) => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td><img src="${item.logo}" alt="Bandiera ${item.gp}" style="height:25px;"/></td>
+            <td>${item.gp}</td>
+            <td>${item.data}</td>
+            <td><a href="#" onclick="showImagePopup('${getImageFilenameFromGP(item.gp)}', '${item.luogo}'); return false;">
+                ${item.luogo}
+            </a></td>
+        `;
+
+        if (index < calendarData.length / 2) {
+            leftBody.appendChild(tr);
+        } else {
+            rightBody.appendChild(tr);
+        }
+    });
+
+    leftTable.appendChild(leftBody);
+    rightTable.appendChild(rightBody);
+
+    container.appendChild(leftTable);
+    container.appendChild(rightTable);
+});
+    function getImageFilenameFromGP(gpName) {
+        return "img/" + "t" + gpName.toLowerCase().replace("gp ", "").replace(/\s+/g, "") + ".jpg";
     }
 
-    // Popola entrambe le tabelle
-    populateTable(firstHalf, tbodyLeft);
-    populateTable(secondHalf, tbodyRight);
-});
 
 function getImageFilenameFromGP(gpName) {
-    return "img/" + "t" + gpName.toLowerCase().replace("gp ", "").replace(/\s+/g, "") + ".jpg";
+    return "img/t" + gpName.toLowerCase().replace("gp ", "").replace(/\s+/g, "") + ".jpg";
 }
+
 
 function showImagePopup(imgSrc, title) {
     const modal = document.getElementById("imageModal");
